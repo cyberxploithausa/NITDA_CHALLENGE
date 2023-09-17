d59dae770dad355dfa68b605fccbc7c0ecb77b5a
cyberxploit <cyberxploit166@gmail.com>
2023-09-17 15:46:32 +0100

    change crawler.py

--- App/src/crawler.py
+++ App/src/crawler.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     num_links_to_crawl = 100
     # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-    start_url = 'http://http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion'
+    start_url = 'http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion'
     keywords = input(
         'Enter a list of keywords to search for, separated by commas: ').split(',')
 
