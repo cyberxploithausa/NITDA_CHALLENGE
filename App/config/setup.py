39a9eb8c478b6f5f659e34f4b46f3c42ad3149e3
cyberxploit <cyberxploit166@gmail.com>
2023-09-19 22:20:24 +0100

    modified setup

--- App/config/setup.py
+++ App/config/setup.py
@@ -2,4 +2,6 @@ import os
 
 
 # Starting Tor service
-os.system('\Desktop\TorBrowser\Browser\Tor\tor.exe' if os.name=='nt' else 'service tor start')
+os.system("C:/Users/cyberxploit/Desktop/Tor Browser/Browser/TorBrowser/Tor/tor.exe" if os.name=='nt' else 'service tor start')
+#os.system("tor.exe" if os.name=='nt' else 'service tor start')
+# C:\Users\cyberxploit\Desktop\Tor Browser\Browser\TorBrowser\Tor
