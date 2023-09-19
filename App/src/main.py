51834d3ee8a1aabca7d9a80ff09737776860ed53
cyberxploit <cyberxploit166@gmail.com>
2023-09-19 11:13:54 +0100

    finaly configure tor

--- App/src/main.py
+++ App/src/main.py
@@ -1,68 +1,79 @@
-import requests
-import time
-from stem import Signal
-from stem.control import Controller
-from bs4 import BeautifulSoup
+# import requests
+# import time
+# from stem import Signal
+# from stem.control import Controller
+# from bs4 import BeautifulSoup
 
 
-def initialize_tor_controller(password):
-    with Controller.from_port(port=9051) as controller:
-        controller.authenticate(password='cyberxploithausa')
-        return controller
+# def initialize_tor_controller(password):
+#     with Controller.from_port(port=9051) as controller:
+#         controller.authenticate(password='cyberxploithausa')
+#         return controller
 
 
-def get_links_from_url(url, headers):
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find_all('a')
-    return [a.get('href') for a in links]
+# def get_links_from_url(url, headers):
+#     response = requests.get(url, headers=headers)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#     links = soup.find_all('a')
+#     return [a.get('href') for a in links]
 
 
-def crawl_links(start_url, num_links_to_crawl, keywords, user_agent):
-    visited = set()
-    queue = [start_url]
-    headers = {'User-Agent': user_agent}
+# def crawl_links(start_url, num_links_to_crawl, keywords, user_agent):
+#     visited = set()
+#     queue = [start_url]
+#     headers = {'User-Agent': user_agent}
 
-    with initialize_tor_controller('3BB5F9D9E5323A9C6060A686DBAAEF03373E83CAD054A0598A3C7A8090') as controller:
-        while queue:
-            link = queue.pop(0)
+#     with initialize_tor_controller('cyberxploithausa') as controller:
+#         while queue:
+#             link = queue.pop(0)
 
-            if link in visited:
-                continue
+#             if link in visited:
+#                 continue
 
-            controller.signal(Signal.NEWNYM)
-            links = get_links_from_url(link, headers)
+#             controller.signal(Signal.NEWNYM)
+#             links = get_links_from_url(link, headers)
 
-            for href in links:
-                if any(keyword in href for keyword in keywords):
-                    queue.append(href)
+#             for href in links:
+#                 if any(keyword in href for keyword in keywords):
+#                     queue.append(href)
 
-            visited.add(link)
-            print(f'Title: {get_page_title(link)}\nURL: {link}')
+#             visited.add(link)
+#             print(f'Title: {get_page_title(link)}\nURL: {link}')
 
-            if len(visited) >= num_links_to_crawl:
-                break
+#             if len(visited) >= num_links_to_crawl:
+#                 break
 
-    return visited
+#     return visited
 
 
-def get_page_title(url, headers):
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    return soup.title.string
+# def get_page_title(url, headers):
+#     response = requests.get(url, headers=headers)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#     return soup.title.string
+
+
+# if __name__ == "__main__":
+#     num_links_to_crawl = 100
+#     # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
+#     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+#     start_url = 'http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion'
+#     keywords = input(
+#         'Enter a list of keywords to search for, separated by commas: ').split(',')
+
+#     crawled_links = crawl_links(
+#         start_url, num_links_to_crawl, keywords, user_agent)
+
+#     print('Visited links:')
+#     for link in crawled_links:
+#         print(link)
+
+
+
+import crawler
+import tor_config
+
 
 
 if __name__ == "__main__":
-    num_links_to_crawl = 100
-    # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-    start_url = 'http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion'
-    keywords = input(
-        'Enter a list of keywords to search for, separated by commas: ').split(',')
+    tor_config.torCon()
 
-    crawled_links = crawl_links(
-        start_url, num_links_to_crawl, keywords, user_agent)
-
-    print('Visited links:')
-    for link in crawled_links:
-        print(link)
