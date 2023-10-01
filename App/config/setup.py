85060aeae54cf49cf2370bf84a2c7e7f12bfe615
cyberxploit <cyberxploit166@gmail.com>
2023-10-01 22:45:36 +0100

    modify main

--- App/config/setup.py
+++ App/config/setup.py
@@ -1,8 +1,5 @@
 import os
 
-
-
-
 def start_tor():
     # Starting Tor service
     #os.system("C:/Users/cyberxploit/Desktop/Tor Browser/Browser/TorBrowser/Tor/tor.exe" if os.name=='nt' else 'service tor start')
